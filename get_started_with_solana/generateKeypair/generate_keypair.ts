67c1c73b54ae54673e8d8ec5dbae2c658c8aee2e
ElDepFull <85937379+ElDeepFull@users.noreply.github.com>
2024-08-25 19:16:17 +0000

    Initial commit

--- get_started_with_solana/generateKeypair/generate_keypair.ts
+++ get_started_with_solana/generateKeypair/generate_keypair.ts
@@ -1,6 +1,16 @@
-import { Keypair } from "@solana/web3.js";
+//Generate solana keypair to create a wallet
 
-const keypair = Keypair.generate();
+import { Keypair, Connection, clusterApiUrl} from "@solana/web3.js";
 
-console.log(`The public key is: `, keypair.publicKey.toBase58());
-console.log(`The secret key is: `, keypair.secretKey);
+async function create_wallet() {
+    const connection = new Connection(clusterApiUrl("devnet"));
+
+    const keypair = Keypair.generate();
+    const balance = await connection.getBalance(keypair.publicKey);
+
+    console.log(`The public key is: `, keypair.publicKey.toBase58());
+    console.log(`The secret key is: `, keypair.secretKey);
+    console.log(`Address balance is: ${balance} lamports`);
+}
+
+create_wallet();
