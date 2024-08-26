82726e1c303dc9845ed4b3b76ef2aa885ac1f4a4
ElDepFull <85937379+ElDeepFull@users.noreply.github.com>
2024-08-26 15:16:55 +0000

    updated folder order

--- get_started_with_solana/1_generateKeypair/generate_keypair.ts
+++ get_started_with_solana/1_generateKeypair/generate_keypair.ts
@@ -2,7 +2,6 @@
 
 import { Keypair, Connection, clusterApiUrl} from "@solana/web3.js";
 
-
 async function create_wallet() {
     const connection = new Connection(clusterApiUrl("devnet"));
 
@@ -10,14 +9,13 @@ async function create_wallet() {
         const keypair = Keypair.generate();
         const balance = await connection.getBalance(keypair.publicKey);
 
-        console.log(`The public key is: `, keypair.publicKey.toBase58());
-        console.log(`The secret key is: `, keypair.secretKey);
-        console.log(`Address balance is: ${balance} lamports`);
+        console.log(`✅The public key is: `, keypair.publicKey.toBase58());
+        console.log(`✅The secret key is: `, keypair.secretKey);
+        console.log(`✅Address balance is: ${balance} lamports`);
     } catch (error) {
         console.error("An error occurred while creating the wallet:", error);
     }
 }
-
 create_wallet();
 
 
